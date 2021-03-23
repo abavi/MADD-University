58448b5465fc6917e7cd50e387178e9c90b788cf
abavi <andreibavi@yahoo.com>
2021-03-23 14:14:15 +0000

    Login and Register Pages Design.

--- app/src/main/java/com/example/universityapp/MainActivity.java
+++ app/src/main/java/com/example/universityapp/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.universityapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView txtRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtRegister = findViewById(R.id.txtRegister);
+
+        // Send to Register User activity
+        txtRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), RegisterUser.class);
+                startActivity(intent);
+            }
+        });
     }
 }
